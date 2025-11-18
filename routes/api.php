394d62d1d7a309a4df320f4dc329f1eb6319d767
zhafira404 <email-github-zhafirasany@gmail.com>
2025-11-18 11:37:15 +0700

    feat(layanan): setup awal model, controller, dan migrasi layanan

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ServiceController; // <-- 1. IMPORT CONTROLLER-NYA
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// ... (Mungkin ada rute lain di sini) ...
+
+// 2. DAFTARKAN RUTE-NYA SESUAI ENDPOINT
+Route::get('/services', [ServiceController::class, 'index']);
+
+
+// ... (Rute lain di bawah) ...
